10b4a07d62fa9a7a524d0a7715b2c697b2cb6ca2
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-11-27 14:56:08 +0100

    refactor require role middleware

--- back/src/middlewares/require-role.js
+++ back/src/middlewares/require-role.js
@@ -1,11 +1,14 @@
 const User = require("../models/User");
 
-module.exports = function requireRoles(roles) {
-  return function (req, res, next) {
-    if (roles.includes(req.user.role)) {
-      next();
-    } else {
-      res.status(403).send("Forbbiden");
+module.exports = function requireRoles (roles = []) {
+  return (req, res, next) => {
+    const { user } = req;
+
+    // check if user exists and has the required role
+    if (!user || !roles.includes(user.role)) {
+      return res.status(403).send({ message: "Access forbidden: Insufficient role" });
     }
+
+    next();
   };
 };
