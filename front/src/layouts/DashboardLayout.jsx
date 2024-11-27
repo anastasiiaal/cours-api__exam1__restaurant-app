f962591f052f4dd8d42268d1a9b56507e982e35c
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-11-27 10:22:03 +0100

    add logout icon

--- front/src/layouts/DashboardLayout.jsx
+++ front/src/layouts/DashboardLayout.jsx
@@ -38,8 +38,22 @@ const DashboardLayout = () => {
                                 <li
                                     key={item.label}
                                     onClick={item.action}
-                                    className="cursor-pointer py-2 mt-6 px-4 hover:bg-gray-700 rounded"
+                                    className="cursor-pointer py-2 mt-6 px-4 hover:bg-gray-700 rounded flex items-center space-x-3"
                                 >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="w-5 h-5 mr-2"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M15.75 9V5.25a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 006.75 21h6.75a2.25 2.25 0 002.25-2.25V15M9 12h12m0 0l-3-3m3 3l-3 3"
+                                        />
+                                    </svg>
                                     {item.label}
                                 </li>
                             ) : (
