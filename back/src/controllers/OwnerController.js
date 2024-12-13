d5d1be8b3cb6ff5c52129b4f2501c385f971e2e3
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-12-13 11:37:36 +0100

    api: get dish by id(owner) & post new dish

--- back/src/controllers/OwnerController.js
+++ back/src/controllers/OwnerController.js
@@ -47,6 +47,63 @@ router.get("/dishes", async (req, res) => {
     }
 });
 
+
+// create a new dish for owner's restaurant
+router.post("/dish/new", async (req, res) => {
+    const { name, image, price, description } = req.body;
+
+    try {
+        const restaurant = await Restaurant.findOne({
+            where: { ownerId: req.user.id },
+        });
+
+        if (!restaurant) {
+            return res.status(403).send({ message: "Access forbidden: no restaurant found" });
+        }
+
+        const dish = await Dish.create({
+            name,
+            image,
+            price,
+            description,
+            restaurantId: restaurant.id,
+        });
+
+        res.status(201).send({ message: "Dish created successfully:", dish });
+    } catch (error) {
+        console.error("Error creating dish:", error);
+        res.status(500).send({ message: "Internal server error" });
+    }
+});
+
+// get a specific dish (only if it belongs to the owner's restaurant)
+router.get("/dish/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // Find the dish
+        const dish = await Dish.findByPk(id, {
+            include: {
+                model: Restaurant,
+                attributes: ["ownerId"],
+            },
+        });
+
+        if (!dish) {
+            return res.status(404).send({ message: "Dish not found" });
+        }
+
+        if (dish.Restaurant.ownerId !== req.user.id) {
+            return res.status(403).send({ message: "Access forbidden: dish does not belong to your restaurant" });
+        }
+
+        res.status(200).send(dish);
+    } catch (error) {
+        console.error("Error fetching dish:", error);
+        res.status(500).send({ message: "Internal server srror" });
+    }
+});
+
 // fetch all restaurant orders
 router.get("/orders", async (req, res) => {
     try {
--- back/src/models/index.js
+++ back/src/models/index.js
@@ -4,11 +4,11 @@ const Dish = require('./Dish');
 const Order = require('./Order');
 
 // relations between models
-User.hasOne(Restaurant, { as: "restaurant", foreignKey: "ownerId" });
+User.hasOne(Restaurant, { as: "restaurant", foreignKey: "ownerId", onDelete: "CASCADE" });
 Restaurant.belongsTo(User, { as: "owner", foreignKey: "ownerId" });
 
-Restaurant.hasMany(Dish, { foreignKey: "restaurantId" });
+Restaurant.hasMany(Dish, { foreignKey: "restaurantId", onDelete: "CASCADE" });
 Dish.belongsTo(Restaurant, { foreignKey: "restaurantId" });
 
-Restaurant.hasMany(Order, { foreignKey: "restaurantId" });
+Restaurant.hasMany(Order, { foreignKey: "restaurantId", onDelete: "CASCADE" });
 Order.belongsTo(Restaurant, { foreignKey: "restaurantId" });
