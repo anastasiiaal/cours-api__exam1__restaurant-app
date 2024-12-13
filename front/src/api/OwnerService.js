ef3bfc895719c1f8de10848315bbc952b1caa74d
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-12-13 12:33:38 +0100

    add two new fetches on front (fetch owners dishes + post new dish)

--- front/src/api/OwnerService.js
+++ front/src/api/OwnerService.js
@@ -10,6 +10,26 @@ class OwnerService {
             throw error.response?.data?.message || "Failed to fetch restaurant data";
         }
     }
+
+    async getDishes() {
+        try {
+            const response = await axiosInstance.get("/owner/dishes");
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching dishes:", error);
+            throw error;
+        }
+    }
+
+    async createDish(dishData) {
+        try {
+            const response = await axiosInstance.post("/owner/dish/new", dishData);
+            return response.data;
+        } catch (error) {
+            console.error("Error creating new dish:", error);
+            throw error;
+        }
+    }
 }
 
 export default new OwnerService();
